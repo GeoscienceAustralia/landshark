e7405ff949964da997d1ab274fef5c0213ab7804
Sudipta Basak <basaks54321@users.noreply.github.com>
2022-04-05 17:27:51 +1000

    add outputs with and without halfwidth

---
+++ check_nn_keras_reg_hw1/sub_nn_reg.sh
@@ -0,0 +1,27 @@
+landshark-import --nworkers 0 --batch-mb 0.001 tifs \
+    --name sirsam \
+    --ignore-crs  \
+    --continuous ../integration/data/continuous
+
+landshark-import --batch-mb 0.001 targets \
+  --shapefile ../integration/data/targets/geochem_sites.shp \
+  --name Na_ppm_i_1 \
+  --record Na_ppm_i_1 \
+  --dtype continuous
+
+
+rm -rf traintest_sirsam_fold1of10
+landshark-extract --nworkers 0 --batch-mb 0.001 traintest \
+  --features features_sirsam.hdf5 \
+  --split 1 10 \
+  --targets targets_Na_ppm_i_1.hdf5 \
+  --name sirsam \
+  --halfwidth 1
+
+rm -rf nn_regression_keras_model_1of10
+landshark --keras-model train \
+  --data traintest_sirsam_fold1of10 \
+  --config nn_regression_keras.py \
+  --epochs 20 \
+  --iterations 500
+
