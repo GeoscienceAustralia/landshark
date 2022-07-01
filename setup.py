197935c39a20d44ab1412ebfb9ef3b3c34cd2a0d
Sudipta Basak <basaks@gmail.com>
2022-07-01 16:48:12 +1000

    lock some more packages

--- setup.py
+++ setup.py
@@ -53,7 +53,9 @@ setup(
         "tqdm>=4.19.6",
         "scikit-learn>=0.20.0",
         "tensorflow==2.6.0",
+        "keras==2.6.0",
         "tensorflow_probability>=0.11",
+        "pydot~=1.4.2"
     ],
     extras_require={
         "dev": [
