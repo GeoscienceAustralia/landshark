8f2962f5ff04a9efee45cae52d39269f9638c5ff
Sudipta Basak <basaks@gmail.com>
2022-11-05 16:17:22 +1100

    more automation scripts

--- check_tfp_nn_keras_reg_hw1/pred_parts.sh
+++ check_tfp_nn_keras_reg_hw1/pred_parts.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
+#PBS -l ncpus=48
+#PBS -l mem=192GB
+#PBS -l jobfs=10GB
+#PBS -q normal
+#PBS -P ge3
+#PBS -l walltime=2:00:00
+#PBS -l storage=gdata/dg9+gdata/dz56+gdata/ge3
+#PBS -l wd
+#PBS -j oe
+set -e
+export name="lake_eyre"
+export halfwidth=1
+export config="./nn_regression_keras_global_local.py"
+
+export config_basename=${config##*/}
+export config_stem=${config_basename%.*}
+echo name: $name, halfwidth: $halfwidth
+echo using config: $config,  basename: $config_basename, stem: $config_stem
+
 pid=$(grep ^Pid /proc/self/status)
 corelist=$(grep Cpus_allowed_list: /proc/self/status | awk '{print $2}')
 host=$(hostname | sed 's/.gadi.nci.org.au//g')
 echo subtask $1 running in $pid using cores $corelist on compute node $host
 
+module load tensorflow/2.6.0 python3/3.9.2 openmpi/4.1.1 gdal/3.0.2 parallel
+source /g/data/ge3/sudipta/venvs/land3p9n/bin/activate
 
-module load python3/3.9.2 gdal/3.5.0
-source /g/data/ge3/sudipta/venvs/nompiland/bin/activate
-export PYTHONPATH=/apps/gdal/3.5.0/lib/python3.9/site-packages:/g/data/ge3/sudipta/venvs/nompiland/lib/python3.9/site-packages/:/apps/python3/3.9.2/lib/python3.9/site-packages/
+export PYTHONPATH=/apps/tensorflow/2.6.0/lib/python3.9/site-packages:/g/data/ge3/sudipta/venvs/land3p9n/lib/python3.9/site-packages/:/apps/python3/3.9.2/lib/python3.9/site-packages/
 
 
 function query_predict {
     echo starting query and preict $1 of $2
     n=$1
     N=$2
-    landshark-extract --nworkers 0 --batch-mb 0.1 query \
-        --features ./features_wa_cond.hdf5 \
-        --strip ${n} ${N} --name wa_cond \
-        --halfwidth 1
-    landshark --keras-model --batch-mb 0.1 predict \
-        --config ./nn_regression_keras_global_local.py \
-        --checkpoint ./nn_regression_keras_global_local_model_6of10 \
-        --data query_wa_cond_strip${n}of${N}
+    mkdir -p query_${name}_strip${1}of${2}
+    landshark-extract --nworkers 1 --batch-mb 0.1 query \
+        --features ./features_${name}.hdf5 \
+        --strip ${n} ${N} --name ${name} \
+        --halfwidth ${halfwidth}
+    landshark --keras-model --batch-mb 1 predict \
+      	--proba true \
+        --config ${config} \
+        --checkpoint ${config_stem}_model_1of10 \
+        --data query_${name}_strip${n}of${N}
     echo done query and preict $1 of $2
+    rm -r query_${name}_strip${1}of${2}  # prediction written. Remove query
 }
 export -f query_predict
--- check_tfp_nn_keras_reg_hw1/sub_nn_reg_all.sh
+++ check_tfp_nn_keras_reg_hw1/sub_nn_reg_all.sh
@@ -15,16 +15,36 @@ landshark-extract --nworkers 0 --batch-mb 0.01 traintest \
   --split 1 10 \
   --targets targets_sirsam.hdf5 \
   --name sirsam \
-  --halfwidth 0
+  --halfwidth 1
 
 
-# proba = True
+## proba = True
+landshark --keras-model train \
+  --trainvalidation false \
+  --data traintest_sirsam_fold1of10/ \
+  --config nn_regression_keras_global_local.py  \
+  --epochs 10 --batchsize 153 --iterations 10
+#
+landshark-extract --batch-mb 0.01 query \
+    --features features_sirsam.hdf5 \
+    --strip 5 200 \
+    --name sirsam \
+    --halfwidth 1
+
+landshark -v DEBUG --keras-model --batch-mb 0.001 predict \
+    --proba true \
+    --config nn_regression_keras_global_local.py \
+    --checkpoint nn_regression_keras_global_local_model_1of10 \
+    --data query_sirsam_strip5of200 \
+    --pred_ensemble_size 12
+
+# non-proba
 #landshark --keras-model train \
 #  --trainvalidation false \
 #  --data traintest_sirsam_fold1of10/ \
-#  --config nn_regression_keras_global_local.py  \
+#  --config nn_regression_keras.py  \
 #  --epochs 10 \
-#  --iterations 10
+#  --iterations 53 --batchsize 100
 #
 #landshark-extract --batch-mb 0.01 query \
 #    --features features_sirsam.hdf5 \
@@ -33,28 +53,7 @@ landshark-extract --nworkers 0 --batch-mb 0.01 traintest \
 #    --halfwidth 0
 #
 #landshark --keras-model --batch-mb 0.01 predict \
-#    --proba true \
-#    --config nn_regression_keras_global_local.py \
-#    --checkpoint nn_regression_keras_global_local_model_1of10 \
+#    --proba false \
+#    --config nn_regression_keras.py \
+#    --checkpoint nn_regression_keras_model_1of10 \
 #    --data query_sirsam_strip5of200
-
-
-# non-proba
-landshark --keras-model train \
-  --trainvalidation false \
-  --data traintest_sirsam_fold1of10/ \
-  --config nn_regression_keras.py  \
-  --epochs 10 \
-  --iterations 10
-
-landshark-extract --batch-mb 0.01 query \
-    --features features_sirsam.hdf5 \
-    --strip 5 200 \
-    --name sirsam \
-    --halfwidth 0
-
-landshark --keras-model --batch-mb 0.01 predict \
-    --proba false \
-    --config nn_regression_keras.py \
-    --checkpoint nn_regression_keras_model_1of10 \
-    --data query_sirsam_strip5of200
