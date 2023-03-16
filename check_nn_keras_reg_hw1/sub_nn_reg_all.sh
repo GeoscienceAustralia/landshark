1c6d9f4da04813fd632680d46509731dcdcd14b8
Sudipta Basak <basaks@gmail.com>
2023-03-17 05:30:26 +1100

    solid scripts for non proba models including oos

--- check_nn_keras_reg_hw1/sub_nn_reg_all.sh
+++ check_nn_keras_reg_hw1/sub_nn_reg_all.sh
@@ -1,40 +1,41 @@
 set -e
-
+export name="sirsam"
 # extract features
 landshark-import --nworkers 0 --batch-mb 0.001 tifs \
-    --name sirsam \
+    --name ${name} \
     --ignore-crs  \
     --continuous ../integration/data/continuous
 
 # extract targets
 landshark-import --batch-mb 0.001 targets \
   --shapefile ../integration/data/targets/geochem_sites.shp \
-  --name Na_ppm_i_1 \
+  --name ${name} \
   --record Na_ppm_i_1 \
+  --record Zr_ppm_i_1 \
   --dtype continuous
 
 # create train/test data split
 landshark-extract --nworkers 0 --batch-mb 0.001 traintest \
-  --features features_sirsam.hdf5 \
+  --features features_${name}.hdf5 \
   --split 1 10 \
-  --targets targets_Na_ppm_i_1.hdf5 \
-  --name sirsam \
+  --targets targets_${name}.hdf5 \
+  --name ${name} \
   --halfwidth 1
 
 # extract validation data - note I am using the same targets as targets and validation here
 # targets and validation hdf5 files could be different with a different validation shapefile/hdf5
 landshark-extract --nworkers 0 --batch-mb 0.001 trainvalidate \
-  --features features_sirsam.hdf5 \
-  --targets targets_Na_ppm_i_1.hdf5 \
-  --validation targets_Na_ppm_i_1.hdf5 \
-  --name sirsam \
+  --features features_${name}.hdf5 \
+  --targets targets_${name}.hdf5 \
+  --validation targets_${name}.hdf5 \
+  --name ${name} \
   --halfwidth 1
 
 
 # train using train test split
 landshark --keras-model train \
   --trainvalidation false \
-  --data traintest_sirsam_fold1of10/ \
+  --data traintest_${name}_fold1of10/ \
   --config nn_regression_keras.py  \
   --epochs 20 \
   --iterations 5
@@ -42,7 +43,7 @@ landshark --keras-model train \
 # train using validation data
 landshark --keras-model train \
   --trainvalidation true \
-  --data trainvalidate_sirsam/   \
+  --data trainvalidate_${name}/   \
   --config nn_regression_keras.py   \
   --epochs 20 \
   --iterations 5
@@ -50,31 +51,30 @@ landshark --keras-model train \
 
 # extract query for prediction
 landshark-extract query \
-    --features features_sirsam.hdf5 \
+    --features features_${name}.hdf5 \
     --strip 5 10 \
-    --name sirsam \
+    --name ${name} \
     --halfwidth 1
 
 # predict using train/test data based training
 landshark --keras-model --batch-mb 0.001 predict \
     --config nn_regression_keras.py \
     --checkpoint nn_regression_keras_model_1of10 \
-    --data query_sirsam_strip5of10
+    --data query_${name}_strip5of10
 
 # predict using train/validation data based training
 landshark --keras-model --batch-mb 0.001 predict \
     --config nn_regression_keras.py \
     --checkpoint nn_regression_keras_model_train_validation/ \
-    --data query_sirsam_strip5of10
+    --data query_${name}_strip5of10
 
 
-# oos validation block
-
+# ======================oos validation block =============================
 # import oos data from oos shapefile into target oos hdf5 file
 # note I am using the same shapefile even as oos shapefile here
 landshark-import --batch-mb 0.001 targets \
   --shapefile ../integration/data/targets/geochem_sites.shp \
-  --name sirsam_oos \
+  --name ${name}_oos \
   --record Na_ppm_i_1 \
   --dtype continuous
 
@@ -82,10 +82,10 @@ landshark-import --batch-mb 0.001 targets \
 # extract oos targets into tfrecord
 # TODO: can avoid saving one of train.xxx.tfrecord and test.xxx.tfrecord
 landshark-extract --nworkers 0 --batch-mb 0.01 traintest \
-  --features features_sirsam.hdf5 \
+  --features features_${name}.hdf5 \
   --split 1 1 \
-  --targets targets_sirsam_oos.hdf5 \
-  --name sirsam_oos \
+  --targets targets_${name}_oos.hdf5 \
+  --name ${name}_oos \
   --halfwidth 1
 
 # predict oos score
@@ -93,5 +93,5 @@ landshark -v DEBUG --keras-model --batch-mb 0.001 predict_oos \
     --proba false \
     --config nn_regression_keras.py \
     --checkpoint nn_regression_keras_model_1of10 \
-    --data traintest_sirsam_oos_fold1of1 \
+    --data traintest_${name}_oos_fold1of1 \
     --pred_ensemble_size 12000000  # not used
