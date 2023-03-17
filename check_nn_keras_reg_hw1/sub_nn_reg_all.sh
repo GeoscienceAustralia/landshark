740103346bb3fcc515c25ab917d8bafeebe5cb88
Sudipta Basak <basaks@gmail.com>
2023-03-18 06:36:25 +1100

    make file automation for keras model including validation and oos

--- check_nn_keras_reg_hw1/sub_nn_reg_all.sh
+++ check_nn_keras_reg_hw1/sub_nn_reg_all.sh
@@ -11,23 +11,30 @@ landshark-import --batch-mb 0.001 targets \
   --shapefile ../integration/data/targets/geochem_sites.shp \
   --name ${name} \
   --record Na_ppm_i_1 \
-  --record Zr_ppm_i_1 \
-  --dtype continuous
+  --dtype continuous \
+  --record Zr_ppm_i_1
+
+
+# extract query for prediction
+landshark-extract query \
+    --features features_${name}.hdf5 \
+    --strip 5 10 \
+    --name ${name} \
+    --halfwidth 1
+
+
+exit 0
+
+echo "===========entering train test block ============================"
+echo "===========entering train test block ============================"
+echo "===========entering train test block ============================"
+echo "===========entering train test block ============================"
 
 # create train/test data split
 landshark-extract --nworkers 0 --batch-mb 0.001 traintest \
   --features features_${name}.hdf5 \
   --split 1 10 \
   --targets targets_${name}.hdf5 \
-  --name ${name} \
-  --halfwidth 1
-
-# extract validation data - note I am using the same targets as targets and validation here
-# targets and validation hdf5 files could be different with a different validation shapefile/hdf5
-landshark-extract --nworkers 0 --batch-mb 0.001 trainvalidate \
-  --features features_${name}.hdf5 \
-  --targets targets_${name}.hdf5 \
-  --validation targets_${name}.hdf5 \
   --name ${name} \
   --halfwidth 1
 
@@ -40,6 +47,38 @@ landshark --keras-model train \
   --epochs 20 \
   --iterations 5
 
+
+# predict using train/test data based training
+landshark --keras-model --batch-mb 0.001 predict \
+    --config nn_regression_keras.py \
+    --checkpoint nn_regression_keras_model_1of10 \
+    --data query_${name}_strip5of10
+
+# use validation shapefile to extract a validation
+
+# extract targets
+
+echo "===========entering validation training block ============================"
+echo "===========entering validation training block ============================"
+echo "===========entering validation training block ============================"
+echo "===========entering validation training block ============================"
+
+landshark-import --batch-mb 0.001 targets \
+  --shapefile ../integration/data/targets/geochem_sites.shp \
+  --name validation_${name} \
+  --record Na_ppm_i_1 \
+  --record Zr_ppm_i_1 \
+  --dtype continuous
+
+# extract validation data - note I am using the same targets as targets and validation here
+# targets and validation hdf5 files could be different with a different validation shapefile/hdf5
+landshark-extract --nworkers 0 --batch-mb 0.001 trainvalidate \
+  --features features_${name}.hdf5 \
+  --targets targets_${name}.hdf5 \
+  --validation targets_validation_${name}.hdf5 \
+  --name ${name} \
+  --halfwidth 1
+
 # train using validation data
 landshark --keras-model train \
   --trainvalidation true \
@@ -47,20 +86,6 @@ landshark --keras-model train \
   --config nn_regression_keras.py   \
   --epochs 20 \
   --iterations 5
-
-
-# extract query for prediction
-landshark-extract query \
-    --features features_${name}.hdf5 \
-    --strip 5 10 \
-    --name ${name} \
-    --halfwidth 1
-
-# predict using train/test data based training
-landshark --keras-model --batch-mb 0.001 predict \
-    --config nn_regression_keras.py \
-    --checkpoint nn_regression_keras_model_1of10 \
-    --data query_${name}_strip5of10
 
 # predict using train/validation data based training
 landshark --keras-model --batch-mb 0.001 predict \
@@ -72,12 +97,17 @@ landshark --keras-model --batch-mb 0.001 predict \
 # ======================oos validation block =============================
 # import oos data from oos shapefile into target oos hdf5 file
 # note I am using the same shapefile even as oos shapefile here
+echo "===========entering oos validation block ============================"
+echo "===========entering oos validation block ============================"
+echo "===========entering oos validation block ============================"
+echo "===========entering oos validation block ============================"
+
 landshark-import --batch-mb 0.001 targets \
   --shapefile ../integration/data/targets/geochem_sites.shp \
   --name ${name}_oos \
   --record Na_ppm_i_1 \
+  --record Zr_ppm_i_1 \
   --dtype continuous
-
 
 # extract oos targets into tfrecord
 # TODO: can avoid saving one of train.xxx.tfrecord and test.xxx.tfrecord
